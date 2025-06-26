fd1579b6ba3f980760978f5a5685fa03efbabde9
nathanenriico <enrico13ita@gmail.com>
2025-06-26 15:23:39 -0300

    feat: sistema de autenticação moderno com verificação de email e criação/alteração de conta

--- tcc-facul-main/login-tela/login/twilio-config.js
+++
@@ -1,6 +0,0 @@
-// Configuração do Twilio para envio de SMS
-export const twilioConfig = {
-  accountSid: 'ACd999e382829f59614936da2f1734d7b2',
-  authToken: '44ea3bd2250c85d91ec69979cd4ac919',
-  phoneNumber: '+15076363592'  // Número Twilio de origem (deve ser um número Twilio válido)
-};
